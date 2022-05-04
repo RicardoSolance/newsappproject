2c76ef69d09493170ca0f09a9c23dc3f383ecc0e
Ricardo <Ricardontutumu@gmail.com>
2022-05-04 06:58:52 +0200

    PRINTIGN DONE, AWAIT CREATE

--- src/components/Main/Home/Form/Form.jsx
+++ src/components/Main/Home/Form/Form.jsx
@@ -1,8 +1,54 @@
 import React, { Component } from "react";
+import {Navigate} from "react-router-dom"
+
 
 class Form extends Component {
+   constructor(props) {
+     super(props)
+   
+     this.state = {
+       mynews : [],
+       redirect: false
+      
+     }
+     this.handleSubmit.bind(this);
+   }
+
+  handleSubmit = (e) => {
+    
+    e.preventDefault();
+    const News = {
+      title: e.target.title.value,
+      info: e.target.info.value,
+      author: e.target.author.value,
+      source: e.target.source.value,
+      image: e.target.image.value,
+      url: e.target.url.value,
+    }
+
+    this.props.updateNews(News)
+    this.setState({ mynews: News });
+  }
+ 
   render() {
-    return <div>Form</div>;
+    const { redirect } = this.state;
+    
+    if (redirect) {
+      return <Navigate to='/listnews'/>
+    }
+    
+    return <div className="formulario" >
+
+      <form action="" onSubmit={this.handleSubmit}>
+        <input name="title" type="text" placeholder="News title" />
+        <input name="author" type="text" placeholder="Authors name" />
+        <input name="source" type="text" placeholder="Fuente de la noticia" />
+        <input name="url" type="text" placeholder="url noticia" />
+        <input name="image" type="text" placeholder="url imagen" />
+        <textarea name="info" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Crear noticia</button>
+      </form>
+    </div>;
   }
 }
 
--- src/components/Main/Home/ListNews/ListNews.jsx
+++ src/components/Main/Home/ListNews/ListNews.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Cards from './Cards';
 
 class ListNews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ListNews: this.props.defaultList,
+      News: []
+    }
+    this.updateNews.bind(this);
+  }
+  async componentDidMount(){
+    // const APIKEY = process.env.REACT_APP_API_KEY;
+    const resp = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=DB81RCd7mBPnCwvNINC9wPiRLfWwnWS5`);
+    const data = await resp.data.response.docs.slice(0, 6);
+    const requireData = data.map(article => {
+      return {
+          title: article.headline.main,
+          info: article.abstract,
+          author: article.byline.original,
+          source: article.source,
+          image: article.multimedia[0].url,
+          url: article.web_url
+      }
+  })
+  this.setState({ListNews:requireData});
+  
+  this.updateNews = (News) => {
+    this.setState({ListNews: [...requireData, News]});
+  }
+  }
+
   render() {
-    return <div>ListNews</div>;
+    return <section className="listNews">
+      <h1>Lastes News</h1>
+      <div className="cardlist">
+        { this.state.ListNews.map((News, i) => <Cards value={News} key={i} />)}
+      </div>
+    </section>;
   }
 }
+ListNews.defaultProps = {
+  defaultList: [],
+};
 
 export default ListNews;
